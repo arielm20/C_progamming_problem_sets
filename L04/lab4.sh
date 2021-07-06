b05f6837f54f042c81099baf493960ac90116295
arielm20 <motsenya@mcmaster.ca>
2021-07-06 15:23:23 -0400

    Update lab4.sh

--- L04/lab4.sh
+++ L04/lab4.sh
@@ -5,7 +5,10 @@ WDIR="$1"
 cd "$WDIR"
 
 if [ "$2" -eq 1 ] ; then
-    echo "TODO Objective 1"
+    FILE=/Users/arielmotsenyat/Desktop/School stuff/CS1XC3/L04/file1.txt
+    if test -f "$FILE"; then
+    else
+	
 elif [ "$2" -eq 2 ] ; then
     echo "TODO Objective 2"
 fi
