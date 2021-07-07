faf3759632552601e88d161fd83cb314b73ca8e4
arielm20 <motsenya@mcmaster.ca>
2021-07-07 13:38:47 -0400

    L04 Files updated

--- L04/lab4.sh
+++ L04/lab4.sh
@@ -3,14 +3,31 @@
 # Use First Argument As Working Directory
 WDIR="$1"
 cd "$WDIR"
-
+if [ $# = 3 ] ; then
+    FILE="$3"
+else
+    FILE=file1.txt
+fi
 if [ "$2" -eq 1 ] ; then
-    FILE=/Users/arielmotsenyat/Desktop/School stuff/CS1XC3/L04/file1.txt
-    if test -f "$FILE"; then
-    else
-	
+     if [ ! -e "$FILE" ] ; then
+	touch "$FILE"     
+        echo "$USER" > "$FILE"
+     fi
 elif [ "$2" -eq 2 ] ; then
-    echo "TODO Objective 2"
+     if [ -e "$FILE" ] ; then
+	mkdir backup/"$FILE"
+	mv "$FILE" backup
+     fi
+elif [ "$2" -eq 3 ] ; then
+    if [ -e backup/"$FILE" ] ; then
+	cp backup/"$FILE" .
+	rm backup/"$FILE"
+    fi
 fi
 
+#elif [ $@ -gt 3 ] ; then
+   # touch excess.txt
+    # loop to output each parameter on its own line in excess.txt
+# fi
+     
 echo "Finished"
