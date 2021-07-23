6481dcbe809378da4841e7351e209a735f8f1f32
arielm20 <motsenya@mcmaster.ca>
2021-07-23 12:07:57 -0400

    Create worst.c

---
+++ L08/part4/worst.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main () {
+    int arr[5] = {1,2,3,4,5};
+    printf("a[10] = %d", arr[10]);
+    int x = 5 / 0;
+    printf("5 / 0 = %d", x);
+    bool a = true;
+    printf("%B\n", a++);
+    int i=1;
+    if (i==1) {
+        printf("Hello")
+    } else if (i==1) {
+        printf("World")
+    }
+}
