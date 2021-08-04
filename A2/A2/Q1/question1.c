c31aaf99a6608d6ed08ba7b0a6e9801481423a4d
arielm20 <motsenya@mcmaster.ca>
2021-08-04 11:54:59 -0400

    Update question1.c

--- A2/A2/Q1/question1.c
+++ A2/A2/Q1/question1.c
@@ -5,7 +5,37 @@
 #include <assert.h>
 
 
-float fkgrade(char str[]);
+float fkgrade(char str[]){
+    int sentences=0, i;
+    char last = ' ';
+    for (i=0; str[i] != '\0'; i++){
+        if(str[i] == '.' || str[i] == '?' || str[i] == '!'){
+            sentences++;
+            last = ' ';
+        } else {
+            last = str[i];
+        }
+    }
+    int words=0;
+    for (i=0;str[i] != '\0';i++){
+        if(str[i]==' ' && str[i+1] != ' '){
+            words++;
+        }else if(str[i] == '-' && str[i+1] == '-'){
+            words --;
+        }
+    }
+    words = words + 1;
+    int syllables=0;
+    for (i=0;str[i] != '\0';i++){
+        if((str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' || str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U') && (str[i+1] != 'a' || str[i+1] != 'e' || str[i+1] != 'i' || str[i+1] != 'o' || str[i+1] != 'u' || str[i+1] != 'A' || str[i+1] != 'E' || str[i+1] != 'I' || str[i+1] != 'O' || str[i+1] != 'U')){
+            syllables++;
+        }else if (str[i] == ' '){
+            continue;
+        }
+    }
+    float grade = (0.39*(words/sentences))+(11.8*(syllables/words))-15.59;
+    return grade;
+}
 
 int main() {
     char plato[] = "He who is the real tyrant, whatever men may think, is the real slave, and is obliged to practise the greatest adulation and servility, and to be the flatterer of the vilest of mankind.  He has desires which he is utterly unable to satisfy, and has more wants than any one, and is truly poor, if you know how to inspect the whole soul of him: all his life long he is beset with fear and is full of convulsions and distractions, even as the State which he resembles: and surely the resemblance holds?";
@@ -17,10 +47,10 @@ int main() {
     printf("Plato...\nexpected sentences = 2\n  expected words = 94\n  expected syllables = 135\n");
     float out = fkgrade(plato);
     printf("> Reading Level = %f\n", out);
-    printf("Marcus Aurelius...\n  expected sentences = 7\n   expected words = 104\n    expected syllables = 153\n");
+    printf("Marcus Aurelius...\n  expected sentences = 7\n   expected words = 104\n    expected syllables = 151\n");
     out = fkgrade(aurelius);
     printf("> Reading Level = %f\n", out);
-    printf("Rene Descartes...\n  expected sentences = 3\n   expected words = 67\n    expected syllables = 113\n");
+    printf("Rene Descartes...\n  expected sentences = 3\n   expected words = 67\n    expected syllables = 111\n");
     out = fkgrade(descartes);
     printf("> Reading Level = %f\n", out);
 }
