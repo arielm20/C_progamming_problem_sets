f3841f9c72a892f8203ddbbbf96b4f00ce69683e
arielm20 <motsenya@mcmaster.ca>
2021-08-05 12:05:47 -0400

    Update question3.c

--- A2/A2/Q3/question3.c
+++ A2/A2/Q3/question3.c
@@ -6,27 +6,40 @@
 
 int simpleParse(const char *exp){
 	int i;
-	int sum = 0;
-	for (i=0;i!='\0';i++){
-		if (exp[i]=='+'){
-			sum = (atoi(exp[i-2])+atoi(exp[i+2]));
+	int result = 0;
+	for (i=0;exp[i]!='\0';i++){
+		if (exp[i]==' '){
+			continue;
+		}else if (exp[i]=='+'){
+			int add = atoi(&exp[i-2])+atoi(&exp[i+2]);
+			result += add;
 		}else if(exp[i]=='-'){
-			sum = atoi(exp[i-2]) - atoi(exp[i+2]);
+			int subtract = atoi(&exp[i-2])-atoi(&exp[i+2]);
+			result -= subtract;
+		}else if(exp[i]=='*'){
+			int multiply = atoi(&exp[i-2])*atoi(&exp[i+2]);
+			result *= multiply;
+		}else if(exp[i]=='/'){
+			int divide = atoi(&exp[i-2])/atoi(&exp[i+2]);
+			result /= divide;
+		}else if(exp[i]=='\%'){
+			int remainder = atoi(&exp[i-2])%atoi(&exp[i+2]);
+			result %= remainder;
 		}
 	}
-	return sum;
+	return result;
 }
 
 int main() {
     char exp1[] = "2 + 2";
     printf("%s = %d\n", exp1, simpleParse(exp1));
-	// Expected Value = 4
+	//Expected Value = 4
     char exp2[] = "3 + 7 - 4 * 1";
 	printf("%s = %d\n", exp2, simpleParse(exp2));
 	// Expected Value = 6
     char exp3[] = "9 * 13 / 2 + 18 * 110 - 812 \% 97 + 3";
    	printf("%s = %d\n", exp3, simpleParse(exp3));
-	// Expected Value = 2005
+	//Expected Value = 2005
     
 }
 
